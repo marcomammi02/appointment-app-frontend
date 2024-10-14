0f0f36ee476c2be4f1ab6f8f93bde0a913f6358b
marcomammi02 <marcomammi2002@gmail.com>
2024-10-15 00:07:23 +0200

    Error if more app on same time

--- src/app/components/cms/calendar/calendar.component.ts
+++ src/app/components/cms/calendar/calendar.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {Staff, StaffStore} from "../../../stores/staff.store";
 import {NgClass, NgForOf, NgIf, NgStyle} from "@angular/common";
 import {StoreAppointments} from "../../../stores/appointment.store";
@@ -65,14 +65,22 @@ export class CalendarComponent implements OnInit {
     })
   }
 
-  getAppointmentForTime(hour: string, staff: Staff): any {
-    return this.storeAppointments.appointments.find(app => {
-      return app.staffId === staff.id && toTime(app.startTime) == hour;
-    });
+  getAppointmentForTime(hour: string, staff: Staff): any[] {
+    const apps = this.storeAppointments.appointments.filter(
+      app =>
+        app.staffId == staff.id &&
+        toTime(app.startTime) <= hour &&
+        toTime(app.endTime) > hour
+    );
+    return apps;
+  }
+
+  alreadyCalled(app: any, hour: string) {
+    return toTime(app.startTime) == hour;
   }
 
   goToCreation(hour: string, staff: any) {
-    if (this.getAppointmentForTime(hour, staff)) return
+    if (this.getAppointmentForTime(hour, staff).length > 0) return
     this.storeAppointments.currentHour = hour
     this.storeAppointments.currentStaff = staff
     this.router.navigate([`/private/${this.shopStore.shopId}/appointments/create`])
@@ -92,6 +100,20 @@ export class CalendarComponent implements OnInit {
     return `calc(${pixels}px - 6px)`;
   }
 
+  getAppWidth(app: any, staff: any) {
+    const timeRanges = this.storeAppointments.appointments
+      .filter(existingApp => existingApp.staffId === staff.id && existingApp.id !== app.id )
+      .map(app => ({
+        startTime: timeToMinutes(app.startTime),
+        endTime: timeToMinutes(app.endTime)
+      }));
+    for (let range of timeRanges) {
+      if (timeToMinutes(app.startTime) < range.endTime && timeToMinutes(app.endTime) > range.startTime) {
+        return '3px solid red'
+      }
+    }
+    return 'none'
+  }
+
   protected readonly firstLetter = firstLetter;
-  protected readonly capitalizeFirstLetter = capitalizeFirstLetter;
 }
