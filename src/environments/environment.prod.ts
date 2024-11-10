666dc9f7ac032f329825323007f156a4b909bf32
marcomammi02 <marcomammi2002@gmail.com>
2024-11-10 19:20:31 +0200

    try

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  prod: true,
-  apiUrl: 'https://appointment-app-backend-qfa8.onrender.com'  // Sostituisci con l'URL del backend NestJS
-};
+  prod: false,
+  apiUrl: 'http://localhost:3000'  // Sostituisci con l'URL del backend NestJS
+  }
