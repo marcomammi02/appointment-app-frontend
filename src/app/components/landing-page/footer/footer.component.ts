7a577f77672d44b21d88454163a23891aad7639c
marcomammi02 <marcomammi2002@gmail.com>
2025-01-30 14:28:42 +0100

    footer

--- src/app/components/landing-page/footer/footer.component.ts
+++ src/app/components/landing-page/footer/footer.component.ts
@@ -1,12 +1,52 @@
 import { Component } from '@angular/core';
+import { LandingStore } from '../../../stores/landing.store';
+import { ViewportScroller } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
   imports: [],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.scss'
+  styleUrl: './footer.component.scss',
 })
 export class FooterComponent {
+  constructor(
+    private scroller: ViewportScroller,
+    private router: Router,
+    public landingStore: LandingStore
+  ) {}
 
+  goToSection(label: string) {
+    switch (label) {
+      case 'Prezzi':
+        this.router.navigate(['/']).then(() => {
+          setTimeout(() => {
+            this.scroller.scrollToAnchor('pricing');
+          }, 200);
+        });
+        break;
+      case 'Features':
+        this.router.navigate(['/']).then(() => {
+          setTimeout(() => {
+            this.scroller.scrollToAnchor('features');
+          }, 200);
+        });
+        break;
+      case 'FAQ':
+        this.router.navigate(['/']).then(() => {
+          setTimeout(() => {
+            this.scroller.scrollToAnchor('faqs');
+          }, 200);
+        });
+        break;
+      case 'Recensioni':
+        this.router.navigate(['/']).then(() => {
+          setTimeout(() => {
+            this.scroller.scrollToAnchor('reviews');
+          }, 200);
+        });
+        break;
+    }
+  }
 }
