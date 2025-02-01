1c0d4c26582c4598dc4f496e422ec6ac23c34c76
marcomammi02 <marcomammi2002@gmail.com>
2025-02-01 16:29:37 +0100

    Go to create page control

--- src/app/components/confirm-payment-page/confirm-payment-page.component.ts
+++ src/app/components/confirm-payment-page/confirm-payment-page.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 import { FloatLabelModule } from 'primeng/floatlabel';
 import { PasswordModule } from 'primeng/password';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { InputTextModule } from 'primeng/inputtext';
 import { PrimaryBtnComponent } from '../global/primary-btn/primary-btn.component';
 import { ValidationService } from '../../services/validation.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { PaymentService } from '../../services/payment.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-confirm-payment-page',
@@ -14,35 +22,67 @@ import { ValidationService } from '../../services/validation.service';
     PasswordModule,
     ReactiveFormsModule,
     InputTextModule,
-    PrimaryBtnComponent
+    PrimaryBtnComponent,
   ],
   templateUrl: './confirm-payment-page.component.html',
-  styleUrl: './confirm-payment-page.component.scss'
+  styleUrl: './confirm-payment-page.component.scss',
 })
 export class ConfirmPaymentPageComponent {
   ValidationService: any;
 
   constructor(
-    private formBuilder: FormBuilder
+    private formBuilder: FormBuilder,
+    private route: ActivatedRoute,
+    private paymentService: PaymentService,
+    private router: Router
   ) {}
 
-  form!: FormGroup
+  form!: FormGroup;
 
   ngOnInit() {
-    this.buildForm()
+    // Ottieni il session_id dal parametro dell'URL
+    this.route.queryParams.subscribe((params) => {
+      const sessionId = params['session_id'];
+
+      if (!sessionId) {
+        this.router.navigate(['/']);
+      }
+
+      // Chiamata per verificare il pagamento
+      this.verifyPayment(sessionId);
+    });
+
+    this.buildForm();
+  }
+
+  verifyPayment(sessionId: string) {
+    this.paymentService.verifyPayment(sessionId).subscribe(
+      (res: any) => {
+        // Gestisci la risposta, ad esempio crea un account o mostra un errore
+        console.log('Pagamento verificato con successo', res);
+      },
+      (err: any) => {
+        // Gestisci l'errore, ad esempio mostra un messaggio di errore
+        console.error('Errore nella verifica del pagamento', err);
+        this.router.navigate(['/']);
+      }
+    );
   }
 
   buildForm() {
-    this.form = this.formBuilder.group({
-      name: ['', [Validators.required]],
-      email: ['', [Validators.required, ValidationService.emailValidator]],
-      password: ['', [Validators.required, Validators.minLength(8)]],
-      confirmPassword: ['', Validators.required]
-    }, { validator: ValidationService.checkPasswords });
+    this.form = this.formBuilder.group(
+      {
+        name: ['', [Validators.required]],
+        email: ['', [Validators.required, ValidationService.emailValidator]],
+        password: ['', [Validators.required, Validators.minLength(8)]],
+        confirmPassword: ['', Validators.required],
+      },
+      { validator: ValidationService.checkPasswords }
+    );
   }
 
   create() {
-    if (!this.form.valid) return
-    console.log('Create account')
+    if (!this.form.valid) return;
+    console.log('Create account');
   }
 }
--- src/app/services/payment.service.ts
+++ src/app/services/payment.service.ts
@@ -6,12 +6,18 @@ import { environment } from '../../environments/environment';
   providedIn: 'root',
 })
 export class PaymentService {
-
-  private apiUrl: string = environment.apiUrl + '/payments'
+  private apiUrl: string = environment.apiUrl + '/payments';
 
   constructor(private http: HttpClient) {}
 
   createCheckoutSession(plan: 'monthly' | 'annual' | 'lifetime') {
-    return this.http.post<{ url: string }>(`${this.apiUrl}/checkout-session`, { plan });
+    return this.http.post<{ url: string }>(`${this.apiUrl}/checkout-session`, {
+      plan,
+    });
+  }
+
+  verifyPayment(sessionId: string) {
+    return this.http
+      .get(`${this.apiUrl}/verify-payment?session_id=${sessionId}`)
   }
 }
