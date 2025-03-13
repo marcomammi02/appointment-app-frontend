135d567626a9f1d42b4fc6de43f8da352c7b781a
marcomammi02 <marcomammi2002@gmail.com>
2025-03-13 14:26:33 +0100

    dashboard-blocked page

--- src/app/components/dashboard-blocked/dashboard-blocked.component.ts
+++ src/app/components/dashboard-blocked/dashboard-blocked.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { PrimaryBtnComponent } from "../global/primary-btn/primary-btn.component";
 
 @Component({
   selector: 'app-dashboard-blocked',
   standalone: true,
-  imports: [],
+  imports: [PrimaryBtnComponent],
   templateUrl: './dashboard-blocked.component.html',
   styleUrl: './dashboard-blocked.component.scss'
 })
